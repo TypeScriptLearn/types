bcf24623691fac49bbac4b8ed659d487c10dec35
Valeriy Semenenko <gearmobile@gmail.com>
2024-12-27 22:50:57 +0300

    finish

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,33 @@
 class Dog {
-  constructor(public name: string) {}
+  constructor(readonly name: string) {}
   sayHello(): string {
     return `Dog ${this.name} says woof!`;
   }
 }
 
 class Fish {
-  constructor(public name: string) {}
+  constructor(readonly name: string) {}
   dive(deep: number): string {
-    return `Fish ${this.name} dives ${deep} meters!`;
+    return `Fish ${this.name} dives by ${deep} meters in deep!`;
   }
 }
+
+// объединение пользовательских типов
+type Pet = Dog | Fish;
+
+function talkToPet(pet: Pet): string | null {
+  // сужение типа, проверка на пользовательский тип
+  if (pet instanceof Dog) {
+    return pet.sayHello();
+  }
+
+  // сужение типа, проверка на пользовательский тип
+  if (pet instanceof Fish) {
+    return pet.dive(12);
+  }
+
+  return null;
+}
+
+console.log(talkToPet(new Dog('Fido')));
+console.log(talkToPet(new Fish('Nemo')));
